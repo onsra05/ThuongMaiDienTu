2bafccc392fd4f2cd10d8bd67742187502fa4159
onsra05 <hoanganh54321.oc@gmail.com>
2025-03-14 10:33:17 +0700

    updatee

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export const loginService = async ({ email, password }) => {
     const response = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/auth/signin`, 
-        { email, password },
+        `${process.env.REACT_APP_BASE_URL}/auth/signin`, { email, password },
         {
             headers: {
                 "Content-Type": "application/json",
